fb86a87b55a97b59ba1d6b320cde02998b143561
Tommy Ngo <s3900254@csitprdap03.int.its.rmit.edu.au>
2023-10-26 17:01:07 +1100

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="a3.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Assignment 3</title>
+</head>
+<body>
+    <header class="bg-dark text-white text-center p-4">
+        <h1>Welcome to Assignment 3</h1>
+    </header>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">View All Orders</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="create.php">Create Product</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://github.com/rmit-wp-s2-2023/s3900254-a3">GitHub</a>
+            </li>
+        </ul>
+    </nav>
+    <main class="container mt-4">
+        <!-- Your main content goes here -->
+    </main>
+    <footer class="bg-dark text-white text-center p-3">
+        <h2>Sitemap</h2>
+        <ul>
+            <li><a href="index.php">Assignment 3</a>
+                <ul>
+                    <li><a href="index.php">View All Orders</a></li>
+                    <li><a href="create.php">Create Product</a></li>
+                    <li><a href="https://github.com/rmit-wp-s2-2023/s3900254-a3">GitHub</a>
+                        <ul>
+                            <li><a href="https://github.com/rmit-wp-s2-2023/s3900254-a3">Repository</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </footer>
+</body>
+</html>
